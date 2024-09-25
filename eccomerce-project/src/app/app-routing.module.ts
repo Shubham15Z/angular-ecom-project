9601f0e7858f0be27adf5d5b5df01a18e26e716d
Shubham Zungure <zungureshubham1@gmail.com>
2024-09-25 11:47:14 +0530

    added header nav bar and home, seller component with page not found

--- eccomerce-project/src/app/app-routing.module.ts
+++ eccomerce-project/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { SellerAuthComponent } from './seller-auth/seller-auth.component';
+import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'seller-auth', component: SellerAuthComponent},
+  { path: '**', component: PagenotfoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
