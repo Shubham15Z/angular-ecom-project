ffd185701c9ca9e13e2068a6e92c48c123c02734
Shubham Zungure <zungureshubham1@gmail.com>
2024-10-08 11:15:58 +0530

    changed navbar after seller login

--- eccomerce-project/src/app/header/header.component.ts
+++ eccomerce-project/src/app/header/header.component.ts
@@ -1,10 +1,32 @@
 import { Component } from '@angular/core';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
+
+  menuType: String = 'default'
+  constructor(private route: Router) {}
+
+  ngOnInit(): void {
+
+    
+
+    this.route.events.subscribe((val: any) => {
+      if(val.url){
+        if(localStorage.getItem('seller') && val.url.includes('seller')){
+          console.log("In seller area")
+          this.menuType = 'seller'
+        }
+        else{
+          console.log("Outside seller")
+          this.menuType = 'default'
+        }
+      }
+      
+    })
+  }
 
 }
--- eccomerce-project/src/app/services/seller.service.ts
+++ eccomerce-project/src/app/services/seller.service.ts
@@ -31,13 +31,11 @@ export class SellerService {
     }
   }
 
-  userLogin(data: Login) {
+  userLogin(data: Login): void {
     // console.warn(data);
-    this.http.get(`http://localhost:3000/seller?email=${data.email}&password=${data.password}`,
-      { observe: 'response' }
-    ).subscribe((result: any) => {
+    this.http.get(`http://localhost:3000/seller?email=${data.email}&password=${data.password}`,{ observe: 'response' }).subscribe((result: any) => {
       console.warn(result)
-      if (result && result.body && result.body.length) {
+      if(result && result.body && result.body.length) {
         console.warn('User Logged in');
         localStorage.setItem('seller', JSON.stringify(result.body));
         this.router.navigate(['seller-home']);
